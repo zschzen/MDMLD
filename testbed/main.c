5689fe7f464f70b414abf9d7ec956c8653549f73
Leandro Peres <leandroperes@protonmail.com>
2024-03-26 20:37:26 -0300

    Obj_db calls

--- testbed/main.c
+++ testbed/main.c
@@ -20,16 +20,13 @@ typedef struct game_object
 // another structute example
 typedef struct player
 {
-   char          *name;
-   int            id;
-   unsigned int   health;
-   unsigned int   mana;
-   transform_t    transform;
-   struct player *pNext;
+   game_object_t go;
+   unsigned int  health;
+   unsigned int  mana;
 } player_t;
 
 int
-main()
+main(void)
 {
    // Allocate memory for the structure database
    struct_db_t *struct_db = calloc(1, sizeof(struct_db_t));
@@ -47,17 +44,27 @@ main()
    // Register the player_t structure
    static field_info_t player_fields[] =
    {
-      FIELD_INFO(player_t, name,      CHAR,       0),
-      FIELD_INFO(player_t, id,        INT32,      0),
-      FIELD_INFO(player_t, health,    UINT32,     0),
-      FIELD_INFO(player_t, mana,      UINT32,     0),
-      FIELD_INFO(player_t, transform, OBJ_STRUCT, transform),
-      FIELD_INFO(player_t, pNext,     OBJ_PTR,    player)
+      FIELD_INFO(player_t, go,      OBJ_PTR,  game_object),
+      FIELD_INFO(player_t, health,  UINT32,   0),
+      FIELD_INFO(player_t, mana,    UINT32,   0),
    };
    REG_STRUCT(struct_db, player_t, player_fields);
 
    // Print the structure database
    print_structure_db(struct_db);
+
+   object_db_t *object_db = calloc(1, sizeof(object_db_t));
+   object_db->pDB = struct_db;
+
+   // Allocate memory for the player object
+   player_t *player1 = xcalloc(object_db, "player_t", 1);
+   player_t *player2 = xcalloc(object_db, "player_t", 1);
+
+   // Allocate memory for the game object
+   game_object_t *go1 = xcalloc(object_db, "game_object_t", 2);
+
+   // Print the object database
+   print_object_db(object_db);
 
    return 0;
 }
